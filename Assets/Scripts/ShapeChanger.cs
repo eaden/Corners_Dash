cf6d1524d9cf25cef49ff6a6e886f7c64bd9fe68
Unknown <bennet.meier@gmx.net>
2019-01-26 11:42:42 +0100

    bullets

--- Assets/Scripts/ShapeChanger.cs
+++ Assets/Scripts/ShapeChanger.cs
@@ -9,13 +9,16 @@ public class ShapeChanger : MonoBehaviour {
     [SerializeField]
     private Sprite[] _polygon = new Sprite[6];
     private int i = 0;
+    [SerializeField]
+    private GameObject _bullet;
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Shape")
+        if(collision.gameObject.tag == "Bullet")
         {
             i++;
            GetComponent<SpriteRenderer>().sprite = _polygon[i];
+            Destroy(_bullet);
         }
     }
 }
