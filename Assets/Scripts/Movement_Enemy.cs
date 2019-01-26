326a90da1c655246d77e0e6ba83e30b70309b757
Unknown <kmk@gmx.de>
2019-01-26 21:53:56 +0100

    Prefab Player

---
+++ Assets/Scripts/Movement_Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Enemy : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rigid;
+
+    public float targetTime = 60.0f;
+
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        speed = 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            timerEnded();
+        }
+    }
+
+    private void timerEnded()
+    {
+
+    }
+}
