50215b2d97ad9385bc86c133eef44a4afc62eab9
Unknown <kmk@gmx.de>
2019-01-27 12:31:36 +0100

    Level one almost

--- Assets/Scripts/WallScript.cs
+++ Assets/Scripts/WallScript.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallScript : MonoBehaviour {
+public class WallScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Green_Bullet" || collision.gameObject.tag == "Red_Bullet")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
